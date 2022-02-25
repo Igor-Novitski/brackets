bb10ad1249048f559ab9f22431a8f7d7f30869fa
Igor-Novitski <pro100nke@gmail.com>
2022-02-25 21:26:23 +0300

    fix: fix function check

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
   let result = 0;
+  let x = 0;
   for (let i = 0; i < str.length; i++) {
     for (let j = 0; j < bracketsConfig.length; j++) {
       if (str[i] == bracketsConfig[j][0]) {
@@ -8,10 +9,16 @@ module.exports = function check(str, bracketsConfig) {
       if (str[i] == bracketsConfig[j][1]) {
         result -= 1;
       }
+      if (str[i] == bracketsConfig[j][0] && str[i + 1] == bracketsConfig[j][1]) {
+        x += 1;
+      }
       if (result < 0) {
         return false;
       }
     }
+  }
+  if (x == 0) {
+    return false;
   }
   return ((result == 0) ? true : false);
 }
